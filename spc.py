358c687b696399124adcfe9bf4fbef772e203c6d
sh4dow18 <ramses.solano.arias@gmail.com>
2021-10-19 20:03:23 -0600

    Improving polish calculator

--- spc.py
+++ spc.py
@@ -1,49 +1,48 @@
 from sys import argv
 from argparse import ArgumentParser
-     
 
-def polish_calculator(args, inverted=False):
+
+def polish_calculator(args, reverse=False):
     stack = []
     expression = None
-    if inverted is False:
+    temp_1 = None
+    temp_2 = None
+    number = ""
+    if reverse is False:
         expression = args.polish
+        if expression[0].isdigit() is True:
+            print("The expression entered is not ordinary Polish")
+            exit(1)
         expression = expression[::-1]
     else:
-        expression = args.inverted
-    temp_1 = 0
-    temp_2 = 0
-    space = False
-    digit_space_counters = 1
+        expression = args.reverse
+        if expression[0].isdigit() is False:
+            print("The expression entered is not inverted Polish")
+            exit(1)
     for iterable in range(0, len(expression)):
-        if space is True:
-            if digit_space_counters != 2:
-                digit_space_counters += 1
-            else:
-                space = False
-                digit_space_counters = 1
-        elif expression[iterable].isdigit() is True:
-            stack.append(int(expression[iterable]))
+        if expression[iterable].isdigit() is True:
+            number = number + expression[iterable]
         elif expression[iterable] == ' ':
-            if inverted is False:
-                two_digits_number = expression[iterable + 2] + expression[iterable + 1]
-            else:
-                two_digits_number = expression[iterable + 1] + expression[iterable + 2]
-            stack.append(int(two_digits_number))
-            space = True
-        elif expression[iterable] in ['+', '_', '*', '/']:
-            if inverted is False:
+            if number != "":
+                if reverse is False:
+                    number = number[::-1]
+                number = int(number)
+                stack.append(number)
+                number = ""
+        elif expression[iterable] in ['+', '-', '*', '/']:
+            if reverse is False:
                 temp_1 = stack[-1]
             else:
                 temp_2 = stack[-1]
             stack.pop()
-            if inverted is False:
+            if reverse is False:
                 temp_2 = stack[-1]
             else:
                 temp_1 = stack[-1]
             stack.pop()
             if expression[iterable] == '+':
                 stack.append(temp_1 + temp_2)
-            elif expression[iterable] == '_':
+            elif expression[iterable] == '-':
                 stack.append(temp_1 - temp_2)
             elif expression[iterable] == '*':
                 stack.append(temp_1 * temp_2)
@@ -55,13 +54,13 @@ def polish_calculator(args, inverted=False):
 def main():
     parser = ArgumentParser()
     parser.add_argument("-p", "--polish", help="Operation with Polish Notation")
-    parser.add_argument("-i", "--inverted", help="Operation with Inverted Polish Notation")
+    parser.add_argument("-r", "--reverse", help="Operation with Reverse Polish Notation")
     args = parser.parse_args()
     if len(argv) == 1:
         parser.print_help()
     elif args.polish:
         polish_calculator(args)
-    elif args.inverted:
+    elif args.reverse:
         polish_calculator(args, True)
 
 
